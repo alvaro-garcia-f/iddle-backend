dac1aef3708ac066aad4d0ab48573e85099dddb9
Álvaro García <alvaro.garcia.f@gmail.com>
2020-06-21 17:26:46 +0100

    auth routes, controller created. signUp and logIn functions created and working

---
+++ api/routes/auth.router.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+const {
+  signUp,
+  logIn
+} = require('../controllers/auth.controller')
+
+router.post('/signup', signUp)
+router.post('/login', logIn)
+
+module.exports = router
--- api/routes/router.js
+++ api/routes/router.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 
+const authRouter = require('./auth.router')
 const usersRouter = require('./user.router')
 
+// const { authUser } = require('../utils')
+
 router
+  .use('/auth', authRouter)
   .use('/users', usersRouter)
 
 module.exports = router
