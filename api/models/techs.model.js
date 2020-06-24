02072ddcc5d23f56224dad22b9ba017d468f0c91
Álvaro García <alvaro.garcia.f@gmail.com>
2020-06-24 23:13:43 +0100

    Video Schema + Comment Schema added. No validations

--- api/models/techs.model.js
+++ api/models/techs.model.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const techSchema = new mongoose.Schema({
   name: {
     type: String
+  },
+  clicks: {
+    type: Number
   }
 })
 
--- api/models/videos.model.js
+++ api/models/videos.model.js
@@ -1,7 +1,52 @@
 const mongoose = require('mongoose')
 
-const videoSchema = new mongoose.Schema({
+const commentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  text: {
+    type: String
+  }
+})
 
+const videoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  description: {
+    type: String,
+    required: [true, 'Username is required']
+  },
+  duration: {
+    type: String,
+    required: [true, 'Email is required']
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  }],
+  views: {
+    type: Number
+  },
+  techs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'techs'
+  }],
+  level: {
+    type: String,
+    enum: ['newbie', 'beginner', 'intermediate', 'advance', 'expert']
+  },
+  uploadDate: {
+    type: Date,
+    default: new Date()
+  },
+  comments: [commentSchema]
 })
 
 const videoModel = mongoose.model('user', videoSchema)
