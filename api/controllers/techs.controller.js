f0f75ee8ed8f1bf0f349b56d47c0d0cbd4b2cb6b
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2020-06-26 16:26:06 +0100

    tech controller, model and routes done and working

--- api/controllers/techs.controller.js
+++ api/controllers/techs.controller.js
@@ -1,7 +1,10 @@
 const TechModel = require('../models/techs.model')
 
-function getAllTechs () {
-
+function getAllTechs (req, res) {
+  TechModel
+    .find()
+    .then(response => res.json(response))
+    .catch(err => console.error(err))
 }
 
 module.exports = {
--- api/models/techs.model.js
+++ api/models/techs.model.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose')
 
 const techSchema = new mongoose.Schema({
   name: {
-    type: String
+    type: String,
+    unique: true,
+    required: [true, 'Name is required']
   },
   clicks: {
-    type: Number
+    type: Number,
+    default: 0
   }
 })
 
-const techModel = mongoose.model('tech', techSchema)
+const techModel = mongoose.model('techs', techSchema)
 module.exports = techModel
